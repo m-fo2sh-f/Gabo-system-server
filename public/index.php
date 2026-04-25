4878fef0e5f12ce10bd18c024129a24308624aef
mohamed fouad <m-fo2sh-f@users.noreply.huggingface.co>
2026-04-25 14:37:23 +0000

    Update public/index.php

--- public/index.php
+++ public/index.php
@@ -1,21 +1,17 @@
 <?php
-// إجبار السيرفر على إظهار أي أخطاء
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-    if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-        require $maintenance;
-    }
+require __DIR__.'/../vendor/autoload.php';
 
-    // استدعاء الكومبوزر
-    require __DIR__.'/../vendor/autoload.php';
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-    // تشغيل لارافيل
-    $app = require_once __DIR__.'/../bootstrap/app.php';
-    $app->handleRequest(Request::capture());
+$app->handleRequest(Request::capture());
