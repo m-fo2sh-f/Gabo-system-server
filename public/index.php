d8b220469fd4a553c42f1b6dfeb4860a90525f89
mohamed fouad <m-fo2sh-f@users.noreply.huggingface.co>
2026-04-25 14:49:43 +0000

    Update public/index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,17 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
-
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+try {
+    require __DIR__.'/../vendor/autoload.php';
+    $app = require_once __DIR__.'/../bootstrap/app.php';
+    $app->handleRequest(Request::capture());
+} catch (\Throwable $e) {
+    echo "<h1>🚨 Error Found:</h1>";
+    echo "<b>Message:</b> " . $e->getMessage() . "<br>";
+    echo "<b>File:</b> " . $e->getFile() . " on line " . $e->getLine();
+    exit;
 }
-
-require __DIR__.'/../vendor/autoload.php';
-
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
