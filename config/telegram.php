578ef309923cedef70d1b2589457e4516402074a
Mohamed Fouad <mohamedabc336@gmail.com>
2026-04-26 05:46:39 +0300

    fix: set cloudflare worker proxy as default telegram api url to bypass hugging face restrictions

--- config/telegram.php
+++ config/telegram.php
@@ -4,5 +4,5 @@ return [
     'bot_token' => env('TELEGRAM_BOT_TOKEN') ?: '8707348238:AAHCHiUDSINWxi7hA8RiQLjVPwlJ2LGaMv8',
     'chat_id' => env('TELEGRAM_CHAT_ID') ?: '8615564896',
     'backup_bot_token' => env('TELEGRAM_BACKUP_BOT_TOKEN') ?: '8217630549:AAG2SlipGRN2kQDgGLQHsdEM15jpHbMWUMQ',
-    'api_url' => env('TELEGRAM_API_URL', 'https://api.telegram.org'),
+    'api_url' => env('TELEGRAM_API_URL') ?: 'https://telegram-proxy.mohamedabc336.workers.dev',
 ];
